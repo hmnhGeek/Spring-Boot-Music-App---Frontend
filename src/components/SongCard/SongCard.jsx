4c944cde14e0e21613ef559800af852334dc0b2b
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-15 19:44:05 +0530

    Edit cover modal with API integration

--- src/components/SongCard/SongCard.jsx
+++ src/components/SongCard/SongCard.jsx
@@ -1,10 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SongCard.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faPlay, faTrash } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 
+const EditCoverImageModal = ({ songId, isVisible, onClose, refresh }) => {
+  const [coverImage, setCoverImage] = useState(null);
+
+  const handleCoverImageChange = (event) => {
+    setCoverImage(event.target.files[0]);
+  };
+
+  const handleCoverChangeRequest = async () => {
+    const formData = new FormData();
+    formData.append("coverImagePath ", coverImage); // `newCoverFile` should be the selected image file
+
+    await axios
+      .put(
+        `${process.env.REACT_APP_SONG_API_BASE}/change-cover/${songId}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      )
+      .then(() => refresh())
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    isVisible && (
+      <div className="modal-overlay">
+        <div className="upload-modal-content">
+          <div className="form-group">
+            <label>New Cover Image:</label>
+            <input
+              type="file"
+              onChange={handleCoverImageChange}
+              accept="image/*"
+            />
+          </div>
+          <button className="modal-button" onClick={handleCoverChangeRequest}>
+            Submit
+          </button>
+          <button className="modal-button" onClick={onClose}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    )
+  );
+};
+
 const SongCard = ({ song, selectedSong, handleSongClick, refresh }) => {
+  const [editModalVisible, setEditModalVisible] = useState(false);
+
   const handleSongDelete = () => {
     // Show a confirmation popup
     const confirmDelete = window.confirm(
@@ -24,7 +75,19 @@ const SongCard = ({ song, selectedSong, handleSongClick, refresh }) => {
       key={song.id}
       className={`song-card ${selectedSong?.id === song.id ? "selected" : ""}`}
     >
+      <EditCoverImageModal
+        songId={song.id}
+        isVisible={editModalVisible}
+        onClose={() => setEditModalVisible(false)}
+        refresh={refresh}
+      />
       <div className="song-cover">
+        <button
+          className="edit-button"
+          onClick={() => setEditModalVisible(true)}
+        >
+          <FontAwesomeIcon icon={faEdit} />
+        </button>
         {song.coverImageData && (
           <img
             className="cover-image"
